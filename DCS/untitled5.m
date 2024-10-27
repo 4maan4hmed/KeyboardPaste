4e6e60c6348b45b593a3910cb689c5d5e99d57e6
Amaan Ahmad <150702918+4maan4hmed@users.noreply.github.com>
2024-10-27 20:06:28 +0530

    Delete DCS directory

--- DCS/untitled5.m
+++
@@ -1,7 +0,0 @@
-% Parameters: Use the same settings as the modulation step
-samples_per_bit = 8; % Number of samples per bit used in modulation
-h_index = 0.5;       % Modulation index used in GFSK modulation
-packet_size = 256;   % Packet size used during packetization
-
-% Reconstruct the image from the modulated signal
-demodulate_and_reconstruct_image(modulated_signal, samples_per_bit, h_index, packet_size);
